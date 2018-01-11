8001069cdcbefea19985c62a2c1ecf3168ca5a0e
Julien Levesy <jlevesy@tea-ebook.com>
2018-01-11 19:15:44 +0100

    spit out json when something goes wrong

--- controller/errors.go
+++ controller/errors.go
@@ -9,23 +9,74 @@ import (
 	readstackError "github.com/jlevesy/readstack/error"
 )
 
+const (
+	validationErrorType     = "urn:api:readstack:error:validation-error"
+	jsonErrorType           = "urn:api:readstack:error:json-error"
+	internalServerErrorType = "urn:api:readstack:error:internal-server-error"
+)
+
+type invalidParam struct {
+	Name   string
+	Reason string
+}
+
+type apiError struct {
+	Type          string
+	Title         string
+	InvalidParams []*invalidParam
+}
+
 func HandleError(w http.ResponseWriter, err error) {
 	log.Printf("Handler error: [%T] %s", err, err.Error())
 
 	switch v := err.(type) {
 	case *json.SyntaxError:
-		http.Error(
+		handleError(
 			w,
-			fmt.Sprintf("Failed to decode request %v", v),
+			&apiError{
+				Type:  jsonErrorType,
+				Title: fmt.Sprintf("Failed to decode request : %s", err),
+			},
 			http.StatusUnprocessableEntity,
 		)
 	case *readstackError.ValidationError:
-		http.Error(
-			w,
-			fmt.Sprintf("Failed to validate request %v", err),
-			http.StatusBadRequest,
-		)
+		handleValidationError(w, v, http.StatusBadRequest)
 	default:
-		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		handleError(
+			w,
+			&apiError{
+				Type:  internalServerErrorType,
+				Title: "Something went terribly wrong.",
+			},
+			http.StatusInternalServerError,
+		)
 	}
+}
+
+func handleValidationError(w http.ResponseWriter, err *readstackError.ValidationError, statusCode int) {
+	invalidParams := make([]*invalidParam, len(err.Violations))
+
+	for i, v := range err.Violations {
+		invalidParams[i] = &invalidParam{
+			Name:   v.Name,
+			Reason: v.Reason,
+		}
+	}
+
+	handleError(
+		w,
+		&apiError{
+			Type:          validationErrorType,
+			Title:         "Failed to validate request",
+			InvalidParams: invalidParams,
+		},
+		statusCode,
+	)
+}
+
+func handleError(w http.ResponseWriter, err *apiError, statusCode int) {
+	json.NewEncoder(w).Encode(err)
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(statusCode)
 }
