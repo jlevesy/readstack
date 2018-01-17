288b439e7405dba7c94fe5ee5cf258956c715829
Julien Levesy <jlevesy@gmail.com>
2018-01-17 22:10:11 +0100

    make readstack serve the frontend

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const process = require('process');
+
+const defaultAppUrl = 'http://app:8080'
 
 module.exports = {
   entry: './src/index.js',
@@ -12,9 +15,11 @@ module.exports = {
   ],
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist'
+    port: 8081,
+    host: "0.0.0.0",
+    contentBase: './dist',
     proxy: {
-      '/api/v1": "http://app:8080"
+      "/api/v1": process.env['READSTACK_BACKEND_URL'] || defaultAppUrl
     }
   },
   output: {
