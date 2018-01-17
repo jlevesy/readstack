6d17843356226f13a2537532a1b94aa9134b7781
Julien Levesy <jlevesy@gmail.com>
2018-01-17 22:29:30 +0100

    fix model json encoding

--- model/item.go
+++ model/item.go
@@ -1,9 +1,9 @@
 package model
 
 type Item struct {
-	ID   int64
-	Name string
-	URL  string
+	ID   int64  `json:"id"`
+	Name string `json:"name"`
+	URL  string `json:"url"`
 }
 
 func NewItem(name, URL string) *Item {
