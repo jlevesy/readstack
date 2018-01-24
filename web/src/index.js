bda730eac008183c736df3fea0d045125db39784
pportelette <pierre.portelette@free.fr>
2018-01-24 19:09:43 +0100

    Modification methode ajax passage en Fetch

--- web/src/index.js
+++ web/src/index.js
@@ -1,10 +1,15 @@
 var body = document.body;
 
 var formElt = document.createElement("form");
-var nameInputElt = document.createElement("input");
-var urlInputElt = document.createElement("input");
-var envoyerElt = document.createElement("input");
+formElt.enctype = "application/json";
 
+var nameInputElt = document.createElement("input");
+nameInputElt.name = "name";
+
+var urlInputElt = document.createElement("input");
+urlInputElt.name = "url";
+
+var envoyerElt = document.createElement("input");
 envoyerElt.type = "submit";
 
 formElt.appendChild(nameInputElt);
@@ -13,57 +18,37 @@ formElt.appendChild(envoyerElt);
 
 body.appendChild(formElt);
 
-formElt.addEventListener("submit", function (e) {
-    //e.preventDefault();
-    var newItem = new FormData();
-    ajaxPost("http://localhost:8081/api/v1/item", {"name":"superpierre", "url":"https://supersite.com"}, function (reponse) {console.log(newItem)}, true);
-});
-
-ajaxGet("http://localhost:8081/api/v1/item", function (reponse) {
-    var items = JSON.parse(reponse);
-    items.items.forEach(function (item) {
-        var itemsElt = document.createElement("div");
-        var lienElt = document.createElement("a");
-        lienElt.textContent = item.name;
-        lienElt.href = item.url;
-        itemsElt.appendChild(lienElt);
-        body.appendChild(itemsElt);
+fetch('http://localhost:8081/api/v1/item').then(function(response) {
+    if(response.ok) {
+    response.json().then(function(objectJson) {
+        objectJson.items.forEach(function (item) {
+            var itemsElt = document.createElement("div");
+            var lienElt = document.createElement("a");
+            lienElt.textContent = item.name;
+            lienElt.href = item.url;
+            itemsElt.appendChild(lienElt);
+            body.appendChild(itemsElt);
+        });
     });
-});
-
-
-function ajaxGet(url, callback) {
-    var req = new XMLHttpRequest();
-    req.open("GET", url);
-    req.addEventListener("load", function () {
-        if (req.status >= 200 && req.status < 400) {
-            callback(req.responseText);
-        } else {
-            console.error(req.status + " " + req.statusText + " " + url);
-        }
-    });
-    req.addEventListener("error", function () {
-        console.error("Erreur réseau avec l'URL " + url);
-    });
-    req.send(null);
-}
-
-function ajaxPost(url, data, callback, isJson) {
-    var req = new XMLHttpRequest();
-    req.open("POST", url);
-    req.addEventListener("load", function () {
-        if (req.status >= 200 && req.status < 400) {
-            callback(req.responseText);
-        } else {
-            console.error(req.status + " " + req.statusText + " " + url);
-        }
-    });
-    req.addEventListener("error", function () {
-        console.error("Erreur réseau avec l'URL " + url);
-    });
-    if (isJson) {
-        req.setRequestHeader("Content-Type", "application/json");
-        data = JSON.stringify(data);
+    } else {
+      console.log('Mauvaise réponse du réseau');
     }
-    req.send(data);
-}
+  })
+  .catch(function(error) {
+    console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
+  });
+
+formElt.addEventListener("submit", function (e) {
+    var name = formElt.name.value;
+    var url = formElt.url.value;
+    var newItem = {"name" : name, "url": url};
+
+    fetch("http://localhost:8081/api/v1/item", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newItem)
+    })
+    .then( response =>  response.json() );
+});
