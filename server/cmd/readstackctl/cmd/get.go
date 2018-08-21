05a0af4f2e5861a54960896a50472d7f49047957
Julien Levesy <jlevesy@gmail.com>
2018-08-21 13:46:52 +0200

    move readstackctl to cobra + viper

---
+++ server/cmd/readstackctl/cmd/get.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"text/tabwriter"
+
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+
+	"github.com/jlevesy/readstack/server/api"
+)
+
+// getCmd represents the get command
+var getCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Create a new entry",
+	Run: func(cmd *cobra.Command, args []string) {
+		conn, err := grpc.Dial(*backendURL, grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("could not connect to %s: %v", *backendURL, err)
+		}
+
+		defer conn.Close()
+
+		client := api.NewItemClient(conn)
+
+		res, err := client.Index(context.Background(), &api.IndexRequest{})
+		if err != nil {
+			log.Fatal("could not call index endpoint", err)
+		}
+
+		renderItems(res)
+	},
+}
+
+func renderItems(result *api.IndexResult) {
+	w := tabwriter.NewWriter(os.Stdout, 5, 10, 3, ' ', 0)
+	defer w.Flush()
+
+	fmt.Fprintln(w, "ID\tName\tLink")
+	fmt.Fprintln(w, "--\t----\t----")
+
+	for _, item := range result.GetItems() {
+		fmt.Fprintf(w, "%d\t%s\t%s\n", item.GetId(), item.GetName(), item.GetUrl())
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(getCmd)
+}
--- server/cmd/readstackctl/main.go
+++ server/cmd/readstackctl/main.go
@@ -1,48 +1,9 @@
 package main
 
 import (
-	"context"
-	"flag"
-	"fmt"
-	"log"
-	"os"
-	"text/tabwriter"
-
-	"google.golang.org/grpc"
-
-	"github.com/jlevesy/readstack/server/api"
+	"github.com/jlevesy/readstack/server/cmd/readstackctl/cmd"
 )
 
 func main() {
-	backend := flag.String("b", "localhost:8080", "address of the backend")
-
-	flag.Parse()
-
-	conn, err := grpc.Dial(*backend, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("could not connect to %s: %v", *backend, err)
-	}
-
-	defer conn.Close()
-
-	client := api.NewItemClient(conn)
-
-	res, err := client.Index(context.Background(), &api.IndexRequest{})
-	if err != nil {
-		log.Fatal("Could not call index endpoint", err)
-	}
-
-	renderItems(res)
-}
-
-func renderItems(result *api.IndexResult) {
-	w := tabwriter.NewWriter(os.Stdout, 5, 10, 3, ' ', 0)
-	defer w.Flush()
-
-	fmt.Fprintln(w, "ID\tName\tLink")
-	fmt.Fprintln(w, "--\t----\t----")
-
-	for _, item := range result.GetItems() {
-		fmt.Fprintf(w, "%d\t%s\t%s\n", item.GetId(), item.GetName(), item.GetUrl())
-	}
+	cmd.Execute()
 }
