9a8e0a3b7277f5b9c7a12d2ed268db0905618a6b
Julien Levesy <jlevesy@gmail.com>
2018-08-24 11:18:13 +0200

    PoC of a php client

--- php/client.php
+++ php/client.php
@@ -2,7 +2,21 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$client = new Readstack\Api\ItemClient('localhost:50051', [
+$client = new Api\ItemClient('localhost:8080', [
     'credentials' => Grpc\ChannelCredentials::createInsecure(),
 ]);
 
+
+if (sizeof($argv) === 1 || $argv[1] === 'index') {
+    list($result, $status) = $client->Index(new Api\IndexRequest())->wait();
+
+    foreach($result->getItems() as $item) {
+        printf("%d  === %s === %s\n", $item->getId(), $item->getName(), $item->getUrl());
+    }
+
+    exit(0);
+}
+
+if (sizeof($argv) === 2 && $argv[1] === 'delete') {
+    print("WE SHOULD DELETE HERE");
+}
